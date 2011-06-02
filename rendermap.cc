c20e5967141421a82ef8cf2ffd851e924acf406c
Lukas Niederbremer <Lukas.Niederbremer@hs-osnabrueck.de>
2011-06-02 02:35:53 +0200

    Add licensing information

--- rendermap.cc
+++ rendermap.cc
@@ -1,5 +1,16 @@
 /*
- * main.cc
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <iostream>
 
@@ -13,60 +24,60 @@ using namespace nbt;
 
 struct InvalidMapException
 {
-	// Why not.
+    // Why not.
 };
 
 struct MinecraftMap
 {
-	int16_t width;
-	int16_t height;
-	ByteArray map_data;
+    int16_t width;
+    int16_t height;
+    ByteArray map_data;
 };
 
 struct RGBA
 {
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-	uint8_t a;
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
 };
 
 struct PngFile
 {
-	int16_t width;
-	int16_t height;
-	RGBA* data;
+    int16_t width;
+    int16_t height;
+    RGBA* data;
 };
 
 static RGBA colors[] = {
-	// Transparent
-	{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0},
-	// Grass
-	{89,125,39,255}, {109,153,48,255}, {127,178,56,255}, {109,153,48,255},
-	// Sand
-	{174,164,115,255}, {213,201,140,255}, {247,233,163,255}, {213,201,140,255},
-	// Stone(?)
-	{117,117,117,255}, {144,144,144,255}, {167,167,167,255}, {144,144,144,255},
-	// Lava
-	{180,0,0,255}, {220,0,0,255}, {255,0,0,255}, {220,0,0,255},
-	// Ice
-	{112,112,180,255}, {138,138,220,255}, {160,160,255,255}, {138,138,220,255},
-	// More stone(?)
-	{117,117,117,255}, {144,144,144,255}, {167,167,167,255}, {144,144,144,255},
-	// Leaves
-	{0,87,0,255}, {0,106,0,255}, {0,124,0,255}, {0,106,0,255},
-	// Snow
-	{180,180,180,255}, {220,220,220,255}, {255,255,255,255}, {220,220,220,255},
-	// Yet more stone(?)!
-	{115,118,129,255}, {141,144,158,255}, {164,168,184,255}, {141,144,158,255},
-	// Wood and Dirt
-	{129,74,33,255}, {157,91,40,255}, {183,106,47,255}, {157,91,40,255},
-	// More gray!
-	{79,79,79,255}, {96,96,96,255}, {112,112,112,255}, {96,96,96,255},
-	// Water
-	{45,45,180,255}, {55,55,220,255}, {64,64,255,255}, {55,55,220,255},
-	// Log and Tree
-	{73,58,35,255}, {89,71,43,255}, {104,83,50,255}, {89,71,43,255}
+    // Transparent
+    {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0},
+    // Grass
+    {89,125,39,255}, {109,153,48,255}, {127,178,56,255}, {109,153,48,255},
+    // Sand
+    {174,164,115,255}, {213,201,140,255}, {247,233,163,255}, {213,201,140,255},
+    // Stone(?)
+    {117,117,117,255}, {144,144,144,255}, {167,167,167,255}, {144,144,144,255},
+    // Lava
+    {180,0,0,255}, {220,0,0,255}, {255,0,0,255}, {220,0,0,255},
+    // Ice
+    {112,112,180,255}, {138,138,220,255}, {160,160,255,255}, {138,138,220,255},
+    // More stone(?)
+    {117,117,117,255}, {144,144,144,255}, {167,167,167,255}, {144,144,144,255},
+    // Leaves
+    {0,87,0,255}, {0,106,0,255}, {0,124,0,255}, {0,106,0,255},
+    // Snow
+    {180,180,180,255}, {220,220,220,255}, {255,255,255,255}, {220,220,220,255},
+    // Yet more stone(?)!
+    {115,118,129,255}, {141,144,158,255}, {164,168,184,255}, {141,144,158,255},
+    // Wood and Dirt
+    {129,74,33,255}, {157,91,40,255}, {183,106,47,255}, {157,91,40,255},
+    // More gray!
+    {79,79,79,255}, {96,96,96,255}, {112,112,112,255}, {96,96,96,255},
+    // Water
+    {45,45,180,255}, {55,55,220,255}, {64,64,255,255}, {55,55,220,255},
+    // Log and Tree
+    {73,58,35,255}, {89,71,43,255}, {104,83,50,255}, {89,71,43,255}
 };
 
 
@@ -75,157 +86,157 @@ void write(PngFile&, const string&);
 
 int main(int argc, char** argv)
 {
-	if (argc < 2)
-	{
-		cerr << "Usage: " << argv[0] << " <map_file>" << std::endl;
-		return 1;
-	}
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << " <map_file>" << std::endl;
+        return 1;
+    }
 
-	NbtFile f;
-	MinecraftMap m;
+    NbtFile f;
+    MinecraftMap m;
 
-	try
-	{
-	    f.open(argv[1]);
-		f.read();
-		load(f, m);
-	}
-	catch(GzipIOException &e)
-	{
-		cerr << "Error reading '" << argv[1] << "' (code " << e.getCode() << ")"
-             << endl;
-		return 1;
-	}
-	catch(InvalidMapException &e)
-	{
-		cerr << "Invalid map file!" << endl;
-		return 1;
-	}
+    try
+    {
+        f.open(argv[1]);
+        f.read();
+        load(f, m);
+    }
+    catch(GzipIOException &e)
+    {
+        cerr << "Error reading '" << argv[1] << "' (code " << e.getCode() << ")"
+            << endl;
+        return 1;
+    }
+    catch(InvalidMapException &e)
+    {
+        cerr << "Invalid map file!" << endl;
+        return 1;
+    }
 
-	cout << "Map height: " << m.height << ", width: " << m.width << endl;
-	cout << "Allocating memory..." << endl;
+    cout << "Map height: " << m.height << ", width: " << m.width << endl;
+    cout << "Allocating memory..." << endl;
 
-	uint8_t scale = 4;
-	m.height *= scale;
-	m.width *= scale;
+    uint8_t scale = 4;
+    m.height *= scale;
+    m.width *= scale;
 
-	RGBA* pixel_data = new RGBA[m.height * m.width];
+    RGBA* pixel_data = new RGBA[m.height * m.width];
 
-	cout << "Allocated " << sizeof(RGBA) * m.height*m.width << " bytes" << endl;
+    cout << "Allocated " << sizeof(RGBA) * m.height*m.width << " bytes" << endl;
 
-	/*
-	 * Madness ensues!
-	 */
-	for (int y = 0; y < m.height; y += scale)
-		for (int s1 = 0; s1 < scale; ++s1)
-			for (int x = 0; x < m.width; x += scale)
-				for (int s2 = 0; s2 < scale; ++s2)
-					pixel_data[(y+s1)*m.width + (x+s2)] =
-						colors[m.map_data[(y/scale)*(m.width/scale)+(x/scale)]];
+    /*
+     * Madness ensues!
+     */
+    for (int y = 0; y < m.height; y += scale)
+        for (int s1 = 0; s1 < scale; ++s1)
+            for (int x = 0; x < m.width; x += scale)
+                for (int s2 = 0; s2 < scale; ++s2)
+                    pixel_data[(y+s1)*m.width + (x+s2)] =
+                        colors[m.map_data[(y/scale)*(m.width/scale)+(x/scale)]];
 
-	PngFile pf = { m.width, m.height, pixel_data };
-	string basename(argv[1]);
-	write(pf, basename + ".png");
-	delete[] pixel_data;
+    PngFile pf = { m.width, m.height, pixel_data };
+    string basename(argv[1]);
+    write(pf, basename + ".png");
+    delete[] pixel_data;
 
-	return 0;
+    return 0;
 }
 
 void write(PngFile& f, const string& fn)
 {
-	// TODO: Error handling!
+    // TODO: Error handling!
 
-	png_bytep* row_pointers;
-	png_structp png_ptr;
-	png_infop info_ptr;
+    png_bytep* row_pointers;
+    png_structp png_ptr;
+    png_infop info_ptr;
 
-	png_byte color_type = PNG_COLOR_TYPE_RGBA;
-	png_byte bit_depth  = 8;
+    png_byte color_type = PNG_COLOR_TYPE_RGBA;
+    png_byte bit_depth  = 8;
 
-	FILE* fp = fopen(fn.c_str(), "wb");
+    FILE* fp = fopen(fn.c_str(), "wb");
 
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (!png_ptr)
-		return;
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr)
+        return;
 
-	info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr)
-		return;
+    info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+        return;
 
-	png_init_io(png_ptr, fp);
+    png_init_io(png_ptr, fp);
 
-	png_set_IHDR(png_ptr, info_ptr, f.width, f.height, bit_depth, color_type,
-				 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE,
-				 PNG_FILTER_TYPE_BASE);
-	png_write_info(png_ptr, info_ptr);
+    png_set_IHDR(png_ptr, info_ptr, f.width, f.height, bit_depth, color_type,
+            PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE,
+            PNG_FILTER_TYPE_BASE);
+    png_write_info(png_ptr, info_ptr);
 
-	row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * f.height);
-	for (int y = 0; y < f.height; ++y)
-		row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png_ptr, info_ptr));
+    row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * f.height);
+    for (int y = 0; y < f.height; ++y)
+        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png_ptr, info_ptr));
 
-	for (int y = 0; y < f.height; ++y)
-	{
-		png_byte* row = row_pointers[y];
+    for (int y = 0; y < f.height; ++y)
+    {
+        png_byte* row = row_pointers[y];
 
-		for (int x = 0; x < f.width; ++x)
-		{
-			png_byte* ptr = &(row[x*4]);
+        for (int x = 0; x < f.width; ++x)
+        {
+            png_byte* ptr = &(row[x*4]);
 
-			RGBA* color = &(f.data[y*f.width + x]);
+            RGBA* color = &(f.data[y*f.width + x]);
 
-			ptr[0] = color->r;
-			ptr[1] = color->g;
-			ptr[2] = color->b;
-			ptr[3] = color->a;
-		}
-	}
+            ptr[0] = color->r;
+            ptr[1] = color->g;
+            ptr[2] = color->b;
+            ptr[3] = color->a;
+        }
+    }
 
-	png_write_image(png_ptr, row_pointers);
-	png_write_end(png_ptr, NULL);
+    png_write_image(png_ptr, row_pointers);
+    png_write_end(png_ptr, NULL);
 
-	png_destroy_write_struct(&png_ptr, &info_ptr);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
 
-	for (int y = 0; y < f.height; ++y)
-		free(row_pointers[y]);
+    for (int y = 0; y < f.height; ++y)
+        free(row_pointers[y]);
 
-	free(row_pointers);
-	fclose(fp);
+    free(row_pointers);
+    fclose(fp);
 
-	return;
+    return;
 }
 
 void load(NbtFile& f, MinecraftMap& m)
 {
-	try
-	{
-		TagCompound* file_root = dynamic_cast<TagCompound*>(f.getRoot());
-		if (!file_root)
-			throw InvalidMapException();
+    try
+    {
+        TagCompound* file_root = dynamic_cast<TagCompound*>(f.getRoot());
+        if (!file_root)
+            throw InvalidMapException();
 
-		TagCompound* map_data = dynamic_cast<TagCompound*>(
-                                    file_root->getValueAt("data"));
-		if (!map_data)
-			throw InvalidMapException();
+        TagCompound* map_data = dynamic_cast<TagCompound*>(
+                file_root->getValueAt("data"));
+        if (!map_data)
+            throw InvalidMapException();
 
-		// Gah...
-		TagShort* height = dynamic_cast<TagShort*>(
-                                map_data->getValueAt("height"));
-		TagShort* width = dynamic_cast<TagShort*>(
-                                map_data->getValueAt("width"));
-		TagByteArray* data = dynamic_cast<TagByteArray*>(
-                                map_data->getValueAt("colors"));
+        // Gah...
+        TagShort* height = dynamic_cast<TagShort*>(
+                map_data->getValueAt("height"));
+        TagShort* width = dynamic_cast<TagShort*>(
+                map_data->getValueAt("width"));
+        TagByteArray* data = dynamic_cast<TagByteArray*>(
+                map_data->getValueAt("colors"));
 
-		if (!width || !height || !data)
-			throw InvalidMapException();
+        if (!width || !height || !data)
+            throw InvalidMapException();
 
-		m.height = height->getValue();
-		m.width  = width->getValue();
-		m.map_data = data->getValue();
-	}
-	catch (KeyNotFoundException& e)
-	{
-		throw InvalidMapException();
-	}
+        m.height = height->getValue();
+        m.width  = width->getValue();
+        m.map_data = data->getValue();
+    }
+    catch (KeyNotFoundException& e)
+    {
+        throw InvalidMapException();
+    }
 
-	return;
+    return;
 }
